591ec521f254fa3f353d83614497a94fe79951f5
QueenEM <111599897+QueenEM@users.noreply.github.com>
2023-04-10 10:43:07 +0100

    Create profile.cpp

---
+++ profile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "profile.hpp"
+
+Profile::Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns)
+  : name(new_name), age(new_age), city(new_city), country(new_country), pronouns(new_pronouns) {
+ 
+}
+void Profile::add_hobby(std::string new_hobby) {
+    hobbies.push_back(new_hobby);
+ 
+}
